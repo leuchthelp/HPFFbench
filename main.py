3366bfa24211e49f8542c4e75ea02f895bc35c09
leucht <lucas.philipp@st.ovgu.de>
2025-07-29 17:29:49 +0200

    Refactor runners and separat for better clarify
    Former-commit-id: e4d7c87dc42b73f73e15b76ee87b4ada0be33b0b
    Former-commit-id: 9b54063c698248e06c685775c0c10b13ef375c11 [formerly aba10cd6e79cf556220f117332d4e2257273cbc9] [formerly 3b7bc79236dc00253135bd3923bb26fa24ecc3a5 [formerly 767e4cc8065bb442448d204544bf2ce881817ddc]] [formerly 324b950857270bb721dcac8e2901ed50df6f86a7 [formerly 696098bac45360e298ce9da52c7c5deb1d3fbfc5] [formerly e1cafd882c38b55b6168c7d2ff5f34fcd2a96794 [formerly 5723322904f34d02ed67046e58a2220d240e4959]]]
    Former-commit-id: d3dad71795ae9dce91ad0a31e18c5340dfc3468c [formerly ef80b7676e49b243ab45db92ad615fbbaadacf1c] [formerly 332baf0850ff8ffb844f80b5d67cac996bd48b39 [formerly a1d243565c5e62949735a441876d9e2c6531e806]]
    Former-commit-id: 20c27eb363b3038de3ba0b2a8337307486be37c3 [formerly fda8e6826ac86d2b0fcfc8f71c5498a8b63ec9de]
    Former-commit-id: cb6c061a7af588df9f64ae6f7d414e08a9358299
    Former-commit-id: 5ba540f31d6c1e60f6254e62458c3c405402366c [formerly 0e9085ae153fe707137fbd93a8d281c07d797e7d] [formerly b6ee0fdf542f3056a300a1744e32661a746bef45] [formerly 91c7389d6ab6a42557643a4a96e3755ed78f5bc4 [formerly 06aeebd1a0aea4b1b15470d95b41724529a3c2ea] [formerly b6ee0fdf542f3056a300a1744e32661a746bef45 [formerly c925ca62c401f21a0d9db69a3af8a557fdf10691]]]
    Former-commit-id: 168da354592a2314705ca249c34b90706ba0127d [formerly 8d2bf643ca7c60d191005fc6ce626c97bf9433a9] [formerly c898268632698efdfe0c2444ee42415a7db36c20 [formerly 10590e685e380e9234e8c109a9c378c9b0b4987c]]
    Former-commit-id: 3b052058107ad6c6a7795070133a3ecee796119a [formerly c898268632698efdfe0c2444ee42415a7db36c20]
    Former-commit-id: f98c788f398a1f67acb3439051c0c1549e31f9c0

---
+++ main.py
@@ -0,0 +1,245 @@
+from components.python.func.datastruct import bcolors as color
+from components.python.func.dev_utils import calc_chunksize
+from components.python.runners.c.hdf5 import runner_hdf5_c, runner_hdf5_c_parallel, runner_hdf5_c_async, runner_hdf5_c_subfiling
+from components.python.runners.c.netcdf4 import runner_netcdf4_c, runner_netcdf4_c_parallel
+from components.python.runners.python.hdf5 import runner_hdf5, runner_hdf5_parallel
+from components.python.runners.python.netcdf4 import runner_netcdf4, runner_netcdf4_parallel
+from components.python.runners.python.zarr import runner_zarr
+import pandas as pd
+import os, shutil, subprocess, json
+
+
+#paths 
+
+path_to_c = "components/c"
+path_to_python = "components/python"
+path_to_visuals = "components/visualize"
+
+path_c_benchmark = "components/c/benchmarks"
+path_py_benchmark = "components/python/benchmarks"
+
+path_c_datasets = "components/c/data/datasets"
+path_py_datasets = "components/python/data/datasets"
+
+path_c_tmp = "components/c/data/tmp"
+path_py_tmp = "components/python/data/tmp"
+
+path_c_results = "components/c/data/results"
+path_py_results = "components/python/data/results"
+
+path_plotting = "components/visualize/plotting"
+    
+
+def bench_variable(setup, df, variable, iterations, mpi_ranks):
+    index = 0
+    
+    for run in setup.values():
+        
+        shape = run[variable][0]
+        chunks = run[variable][1]
+        
+        total_filesize = calc_chunksize(chunks=shape)
+        
+        size_chunks = [None, None]
+        if len(chunks) != 0:
+            size_chunks = calc_chunksize(chunks=chunks)
+        
+        print(f"{color.WARNING}create Datasets for variable: {variable} with shape: {shape} and chunks: {chunks}, total filesize: {total_filesize[0]} {total_filesize[1]}, filesize per chunk: {size_chunks[0]} {size_chunks[1]}{color.ENDC}")
+        with open(f"{path_py_tmp}/run_config.json", "w") as f:
+            json.dump(run, f)
+        
+        ## create Zarr, NetCDF4 and HDF5 file sequentially 
+        p = subprocess.Popen(f"python {path_py_benchmark}/benchmarks.py -c 1".split())
+        p.wait()
+        
+        p = subprocess.Popen(f"python {path_py_benchmark}/benchmarks.py -c 2".split())
+        p.wait()
+        
+        p = subprocess.Popen(f"python {path_py_benchmark}/benchmarks.py -c 3".split())
+        p.wait()
+        
+        df = runner_zarr(df, shape, chunks, variable, iterations, total_filesize, size_chunks, index)
+        
+        df = runner_netcdf4(df, shape, chunks, variable, iterations, total_filesize, size_chunks, index)
+        
+        df = runner_hdf5(df, shape, chunks, variable, iterations, total_filesize, size_chunks, index)
+        
+        df = runner_netcdf4_parallel(df, shape, chunks, variable, iterations, total_filesize, size_chunks, index, mpi_ranks)
+        
+        df = runner_hdf5_parallel(df, shape, chunks, variable, iterations, total_filesize, size_chunks, index, mpi_ranks)
+        
+        
+        ## delete zarr python file
+        if os.path.exists(f"{path_py_datasets}/test_dataset.zarr"):
+            shutil.rmtree(f"{path_py_datasets}/test_dataset.zarr")
+        
+        ## delete hdf5 python file
+        if os.path.exists(f"{path_py_datasets}/test_dataset.h5"):
+            os.remove(f"{path_py_datasets}/test_dataset.h5")
+        
+        
+        # setup metadata for c-run
+        chunks = []
+        size_chunks = [None, None]
+        total_filesize = calc_chunksize(chunks=shape)
+        
+        df = runner_netcdf4_c(df, shape, chunks, variable, iterations, total_filesize, size_chunks, index)
+        
+        df = runner_hdf5_c(df, shape, chunks, variable, iterations, total_filesize, size_chunks, index)
+        
+        df = runner_netcdf4_c_parallel(df, shape, chunks, variable, iterations, total_filesize, size_chunks, index, mpi_ranks)
+        
+        df = runner_hdf5_c_parallel(df, shape, chunks, variable, iterations, total_filesize, size_chunks, index, mpi_ranks)
+        
+        df = runner_hdf5_c_async(df, shape, chunks, variable, iterations, total_filesize, size_chunks, index, mpi_ranks)
+        
+        df = runner_hdf5_c_subfiling(df, shape, chunks, variable, iterations, total_filesize, size_chunks, index, mpi_ranks)
+        
+        index +=1
+        df.to_json(f"{path_plotting}/plotting_bench_variable.json")
+ 
+ 
+def bench_python(setup, df, variable, iterations, mpi_ranks):
+    index = 0
+    
+    for run in setup.values():
+        
+        shape = run[variable][0]
+        chunks = run[variable][1]
+        
+        total_filesize = calc_chunksize(chunks=shape)
+        
+        size_chunks = [None, None]
+        if len(chunks) != 0:
+            size_chunks = calc_chunksize(chunks=chunks)
+        
+        print(f"{color.WARNING}create Datasets for variable: {variable} with shape: {shape} and chunks: {chunks}, total filesize: {total_filesize[0]} {total_filesize[1]}, filesize per chunk: {size_chunks[0]} {size_chunks[1]}{color.ENDC}")
+        with open(f"{path_py_tmp}/run_config.json", "w") as f:
+            json.dump(run, f)
+        
+        ## create Zarr, NetCDF4 and HDF5 file sequentially 
+        p = subprocess.Popen(f"python {path_py_benchmark}/benchmarks.py -c 1".split())
+        p.wait()
+        
+        p = subprocess.Popen(f"python {path_py_benchmark}/benchmarks.py -c 2".split())
+        p.wait()
+        
+        p = subprocess.Popen(f"python {path_py_benchmark}/benchmarks.py -c 3".split())
+        p.wait()
+        
+        df = runner_zarr(df, shape, chunks, variable, iterations, total_filesize, size_chunks, index)
+        
+        df = runner_netcdf4(df, shape, chunks, variable, iterations, total_filesize, size_chunks, index)
+        
+        df = runner_hdf5(df, shape, chunks, variable, iterations, total_filesize, size_chunks, index)
+        
+        df = runner_netcdf4_parallel(df, shape, chunks, variable, iterations, total_filesize, size_chunks, index, mpi_ranks)
+        
+        df = runner_hdf5_parallel(df, shape, chunks, variable, iterations, total_filesize, size_chunks, index, mpi_ranks)
+        
+        
+        ## delete zarr python file
+        if os.path.exists(f"{path_py_datasets}/test_dataset.zarr"):
+            shutil.rmtree(f"{path_py_datasets}/test_dataset.zarr")
+        
+        ## delete hdf5 python file
+        if os.path.exists(f"{path_py_datasets}/test_dataset.h5"):
+            os.remove(f"{path_py_datasets}/test_dataset.h5")
+        
+        index +=1
+        df.to_json(f"{path_plotting}/plotting_bench_python.json")   
+
+
+def bench_c(setup, df, variable, iterations, mpi_ranks):
+    index = 0
+    
+    for run in setup.values():
+        
+        shape = run[variable][0]
+        chunks = run[variable][1]
+        
+        total_filesize = calc_chunksize(chunks=shape)
+        
+        chunks = []
+        size_chunks = [None, None]
+        if len(chunks) != 0:
+            size_chunks = calc_chunksize(chunks=chunks)
+        
+        print(f"{color.WARNING}create Datasets for variable: {variable} with shape: {shape} and chunks: {chunks}, total filesize: {total_filesize[0]} {total_filesize[1]}, filesize per chunk: {size_chunks[0]} {size_chunks[1]}{color.ENDC}")
+        with open(f"{path_py_tmp}/run_config.json", "w") as f:
+            json.dump(run, f)
+        
+        ## create Zarr, NetCDF4 and HDF5 file sequentially 
+        p = subprocess.Popen(f"python {path_to_python}/benchmarks.py -c 2".split())
+        p.wait()
+        
+        # setup metadata for c-run
+        size_chunks = [None, None]
+        total_filesize = calc_chunksize(chunks=shape)
+        
+        df = runner_netcdf4_c(df, shape, chunks, variable, iterations, total_filesize, size_chunks, index)
+        
+        df = runner_hdf5_c(df, shape, chunks, variable, iterations, total_filesize, size_chunks, index)
+        
+        df = runner_netcdf4_c_parallel(df, shape, chunks, variable, iterations, total_filesize, size_chunks, index, mpi_ranks)
+        
+        df = runner_hdf5_c_parallel(df, shape, chunks, variable, iterations, total_filesize, size_chunks, index, mpi_ranks)
+        
+        df = runner_hdf5_c_async(df, shape, chunks, variable, iterations, total_filesize, size_chunks, index, mpi_ranks)
+        
+        df = runner_hdf5_c_subfiling(df, shape, chunks, variable, iterations, total_filesize, size_chunks, index, mpi_ranks)
+        
+        index +=1
+        df.to_json(f"{path_plotting}/plotting_bench_c.json")
+
+   
+def main():
+    
+    iterations = 5
+    variable = "X"
+    mpi_ranks = 4
+    
+    setup = {
+            "run01": {"X": ([1 * 134217728], [])},
+            "run02": {"X": ([2 * 134217728], [])},
+            "run03": {"X": ([3 * 134217728], [])},
+            #"run04": {"X": ([4 * 134217728], [])},
+            #"run05": {"X": ([5 * 134217728], [])},
+            #"run06": {"X": ([6 * 134217728], [])},
+            #"run07": {"X": ([7 * 134217728], [])},
+            #"run08": {"X": ([8 * 134217728], [])},
+            #"run09": {"X": ([9 * 134217728], [])},
+            #"run10": {"X": ([10 * 134217728], [])},
+            #"run11": {"X": ([20 * 134217728], [])},
+            #"run12": {"X": ([30 * 134217728], [])},
+            #"run13": {"X": ([40 * 134217728], [])},
+            #"run14": {"X": ([50 * 134217728], [])},
+            #"run15": {"X": ([60 * 134217728], [])},
+            #"run16": {"X": ([70 * 134217728], [])},
+            #"run17": {"X": ([80 * 134217728], [])},
+            #"run18": {"X": ([90 * 134217728], [])},
+            #"run19": {"X": ([100 * 134217728], [])},
+            
+            
+            #"run06": {"X": ([faktor * 512, 512, 512], [faktor * 512, 512, 128])},
+            #"run07": {"X": ([faktor * 512, 512, 512], [faktor * 512, 512, 256])},
+            #"run08": {"X": ([faktor * 512, 512, 512], [faktor * 512, 512, 512])},
+            
+            #"run11": {"X": ([512, 512, 512], [512, 1, 1])},
+            #"run12": {"X": ([512, 512, 512], [512, 8, 8])},
+            #"run13": {"X": ([512, 512, 512], [512, 16, 16]), "Y": ([10, 10], [2, 2])},
+            #"run14": {"X": ([512, 512, 512], [512, 32, 32]), "Y": ([10, 10], [2, 2])},
+            #"run15": {"X": ([512, 512, 512], [512, 64, 64]), "Y": ([10, 10], [2, 2])},
+            #"run16": {"X": ([512, 512, 512], [512, 128, 128]), "Y": ([10, 10], [2, 2])},
+            #"run17": {"X": ([512, 512, 512], [512, 256, 256]), "Y": ([10, 10], [2, 2])},
+            #"run18": {"X": ([512, 512, 512], [512, 512, 512]), "Y": ([10, 10], [2, 2])}, 
+            }
+    
+    #bench_variable(setup, pd.DataFrame(), variable=variable, iterations=iterations, mpi_ranks=mpi_ranks)
+    bench_python(setup, pd.DataFrame(), variable=variable, iterations=iterations, mpi_ranks=mpi_ranks)
+    #bench_c(setup, pd.DataFrame(), variable=variable, iterations=iterations, mpi_ranks=mpi_ranks)
+    
+    
+
+if __name__=="__main__":
+    main()
