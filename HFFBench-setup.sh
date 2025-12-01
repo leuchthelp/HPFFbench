206cdc7aff5836c61dc932de4c3773e70d1cfa9d
leucht <lucas.philipp@st.ovgu.de>
2025-12-01 14:19:05 +0100

    Begin updating documentation

--- HFFBench-setup.sh
+++ HFFBench-setup.sh
@@ -5,8 +5,6 @@
 #sudo apt-get update
 #sudo apt-get install bzip2 ca-certificates g++ gcc gfortran git gzip lsb-release patch python3 tar unzip xz-utils zstd
 
-compiler=gcc@11.4.0
-
 # Install spack and add to local shell
 git clone --depth=2 https://github.com/spack/spack.git
 
@@ -25,7 +23,7 @@ spack config add "modules:default:enable:[tcl]"
 spack compilers
 
 # Install and setup "module" and add to local shell
-spack install --fresh lmod %$compiler
+spack install --fresh lmod
 . $(spack location -i lmod)/lmod/lmod/init/profile
 . $HPFF_SPACK_ROOT/share/spack/setup-env.sh
 
