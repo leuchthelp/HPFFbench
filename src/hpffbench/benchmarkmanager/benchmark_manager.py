c1ae4e656de11fb0bcd0663a54722270a638cc7b
leuchthelp <gfpinthehouse@gmail.com>
2026-08-24 13:20:19 +0200

    chore: reset debug helpers

--- src/hpffbench/benchmarkmanager/benchmark_manager.py
+++ src/hpffbench/benchmarkmanager/benchmark_manager.py
@@ -377,7 +377,7 @@ class BenchmarkManager:
             self.__execute_file()
 
         finally:
-            # shutil.rmtree(path=self.dir_path)
+            shutil.rmtree(path=self.dir_path)
             pass
 
         return self.id, self
